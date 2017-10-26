0a9974542ae6033107faa5ee85a5cf9c8acfb97e
Joseph Surla <josephsurla@gmail.com>
2017-10-25 22:50:41 -0700

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,14 @@
-// Code Invitation Component Here
+import React from 'react';
+
+class Invitation extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        <p>{ this.props.children }</p>
+      </div>
+    )
+  }
+}
+
+export default Invitation
